5e04cb8467be7aa8647cacd72299bdba21404c54
Stefan Bente <stefan.bente@th-koeln.de>
2021-07-11 17:57:43 +0200

    created Spring project

---
+++ src/main/java/thkoeln/archilab/st2/exercises/wiederholungss21/WiederholungSs21Application.java
@@ -0,0 +1,13 @@
+package thkoeln.archilab.st2.exercises.wiederholungss21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiederholungSs21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiederholungSs21Application.class, args);
+    }
+
+}
---
+++ src/test/java/thkoeln/archilab/st2/exercises/wiederholungss21/WiederholungSs21ApplicationTests.java
@@ -0,0 +1,13 @@
+package thkoeln.archilab.st2.exercises.wiederholungss21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiederholungSs21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
